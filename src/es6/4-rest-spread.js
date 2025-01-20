53427133766db169755ab243f59152a8878ac305
Jairo Cuartas <jairo.cuartas@openintl.com>
2025-01-20 16:25:17 -0500

    Ejercicio - destructuring en arrays y objetos

---
+++ src/es6/4-rest-spread.js
@@ -0,0 +1,9 @@
+// arrays destructuring
+let fruits = ['Apple', 'Orange', 'Pineapple'];
+let [a, b, c] = fruits;
+console.log(a, b, c);
+
+// Object destructuring
+let user = { username: "Jairo", age: 33 };
+let { username, age } = user;
+console.log(username, age);
