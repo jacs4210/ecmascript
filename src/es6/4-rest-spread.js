afbcaa651b10103afb8319d65e6cd74a0f3b44e3
Jairo Cuartas <jairo.cuartas@openintl.com>
2025-01-20 17:01:09 -0500

    Ejercicios - spread operator y rest params

--- src/es6/4-rest-spread.js
+++ src/es6/4-rest-spread.js
@@ -7,3 +7,19 @@ console.log(a, b, c);
 let user = { username: "Jairo", age: 33 };
 let { username, age } = user;
 console.log(username, age);
+
+// Spread operator
+let person = { name: "Jairo", age: 33 };
+let country = "CO";
+
+let data = { ...person, country };
+console.log(data);
+
+// Rest params
+function sum(num, ...values){
+  console.log(values);
+  console.log(num + values[0]);
+  return num + values[0];
+}
+
+sum(1, 2, 3, 5);
