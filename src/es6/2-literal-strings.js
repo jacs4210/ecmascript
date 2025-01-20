7811e21dcd6e5e1923fdccb3667a4d5e4a1d4966
Jairo Cuartas <jairo.cuartas@openintl.com>
2025-01-20 16:03:45 -0500

    Practica de string literals

---
+++ src/es6/2-literal-strings.js
@@ -0,0 +1,18 @@
+// Concatenación de strings normal
+let hello = 'Hello';
+let world = 'World';
+let epicPhrase = hello + ' ' + world + '!';
+console.log(epicPhrase);
+
+// Template literals
+let epicPhrase2 = `${hello} ${world}!`;
+console.log(epicPhrase2);
+
+// Multi-line strings
+let lorem = 'Esta es una linea \n' + 'esta es otra linea';
+let lorem2 = `Esta es la primera linea
+  esta es la segunda linea
+`;
+
+console.log(lorem);
+console.log(lorem2);
